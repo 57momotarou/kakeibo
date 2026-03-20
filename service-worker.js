748133265140af4590a495b9d477a63b927e46ca
57momotarou <57momoka@gmail.com>
2026-03-20 13:26:55 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 //    ここのバージョン番号を 1つ増やしてください
 //    例: "kakeibo-v2" → "kakeibo-v3"
 // =============================================
-const CACHE_NAME = "kakeibo-v79";
+const CACHE_NAME = "kakeibo-v80";
 
 const urlsToCache = [
   "./",
